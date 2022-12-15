a992e5c8404887a5ead5ba87bb148a8bd22bf53e
rece grisham <rece.grisham@evsck12.com>
2022-12-14 22:05:27 -0600

    Calc Work

--- Android/4FunCalculator/app/src/main/java/com/example/a4funcalculator/MainActivity.java
+++ Android/4FunCalculator/app/src/main/java/com/example/a4funcalculator/MainActivity.java
@@ -1,47 +1,168 @@
 package com.example.a4funcalculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
     TextView output;
-    Button button0, button1, button2, button3, button4, button5, button6, button7, button8, button9, buttonDiv, buttonAdd, buttonEquals, buttonClear, buttonSub;
-
-
+    Button zero, one, two, three, four, five, six, seven, eight, nine, buttonDiv, buttonAdd, buttonEquals, buttonClear, buttonSub, buttonMul;
+    ArrayList<String> equation = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_main);
         output = findViewById(R.id.output);
-        button0 = findViewById(R.id.button0);
-        button1 = findViewById(R.id.button1);
-        button2 = findViewById(R.id.button2);
-        button3 = findViewById(R.id.button3);
-        button4 = findViewById(R.id.button4);
-        button5 = findViewById(R.id.button5);
-        button6 = findViewById(R.id.button6);
-        button7 = findViewById(R.id.button7);
-        button8 = findViewById(R.id.button8);
-        button9 = findViewById(R.id.button9);
-        buttonDiv = findViewById(R.id.buttonDiv);
+        zero = findViewById(R.id.zero);
+        one = findViewById(R.id.one);
+        two = findViewById(R.id.two);
+        three = findViewById(R.id.three);
+        four = findViewById(R.id.four);
+        five = findViewById(R.id.five);
+        six = findViewById(R.id.six);
+        seven = findViewById(R.id.seven);
+        eight = findViewById(R.id.eight);
+        nine = findViewById(R.id.nine);
         buttonAdd = findViewById(R.id.buttonAdd);
+        buttonSub = findViewById(R.id.buttonSub);
+        buttonMul = findViewById(R.id.buttonMul);
+        buttonDiv = findViewById(R.id.buttonDiv);
         buttonEquals = findViewById(R.id.buttonEquals);
         buttonClear = findViewById(R.id.buttonClear);
-        buttonSub = findViewById(R.id.buttonSub);
 
+        zero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                equation.add("0");
+                output.setText(equation.toString());
+            }
+        });
 
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                equation.add("1");
+                output.setText(equation.toString());
+            }
+        });
 
+        two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                equation.add("2");
+                output.setText(equation.toString());
+            }
+        });
 
+        three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                equation.add("3");
+                output.setText(equation.toString());
+            }
+        });
 
+        four.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                equation.add("4");
+                output.setText(equation.toString());
+            }
+        });
 
+        five.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                equation.add("5");
+                output.setText(equation.toString());
+            }
+        });
 
+        six.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                equation.add("6");
+                output.setText(equation.toString());
+            }
+        });
 
+        seven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                equation.add("7");
+                output.setText(equation.toString());
+            }
+        });
 
-        setContentView(R.layout.activity_main);
+        eight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                equation.add("8");
+                output.setText(equation.toString());
+            }
+        });
+
+        nine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                equation.add("9");
+                output.setText(equation.toString());
+            }
+        });
+
+        buttonSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                equation.add("-");
+                output.setText(equation.toString());
+            }
+        });
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                equation.add("+");
+                output.setText(equation.toString());
+            }
+        });
+
+        buttonMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                equation.add("*");
+                output.setText(equation.toString());
+            }
+        });
+
+        buttonDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                equation.add("/");
+                output.setText(equation.toString());
+            }
+        });
+
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                equation.clear();
+                output.setText(equation.toString());
+            }
+        });
+
+        buttonEquals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
